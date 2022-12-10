2d00ce636c14d4c5c790354d1130fc1b88b0de4d
Vicky Kumar <vicky.billionaire5@gmail.com>
2022-12-10 20:32:38 +0530

    Add navigate to a new page

---
+++ lib/category_meals_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  const CategoryMealsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('title'),
+      ),
+      body: Container(
+        child: Text('BINGO!'),
+      ),
+    );
+  }
+}

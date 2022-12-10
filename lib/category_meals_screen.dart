b294e800df81918c10a38c5e760c6193b2083307
Vicky Kumar <vicky.billionaire5@gmail.com>
2022-12-10 21:31:40 +0530

    Add a meal model & data and selecting meals for a chosen category

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class CategoryMealsScreen extends StatelessWidget {
-  const CategoryMealsScreen({super.key});
+import './dummy_data.dart';
 
+class CategoryMealsScreen extends StatelessWidget {
+  // final String categoryId;
+  // final String categoryTitle;
+  // const CategoryMealsScreen(
+  //     {super.key, required this.categoryId, required this.categoryTitle});
+  const CategoryMealsScreen({super.key});
   static const routeName = '/category-meals';
 
   @override
@@ -11,12 +16,18 @@ class CategoryMealsScreen extends StatelessWidget {
         ModalRoute.of(context)?.settings.arguments as Map<String, String>;
     final categoryId = routeArgs['id'];
     final categoryTitle = routeArgs['title'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle!),
       ),
-      body: Container(
-        child: Text('BINGO!!'),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return Text(categoryMeals[index].title);
+        },
+        itemCount: categoryMeals.length,
       ),
     );
   }
